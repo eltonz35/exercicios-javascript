82471560e52aeac062fed01927572db384ce4853
Elthon <eltonsilva.mecatronic@gmail.com>
2022-08-04 16:30:28 -0300

    ex4

---
+++ introducao/ex4.js
@@ -0,0 +1,18 @@
+/*Um novo modelo de carro, super econômico foi lançado.
+Ele faz 20 km com 1 litro de combustível.
+Cada litro de combustível custa R$ 5,00.
+
+Faça um programa que pergunte ao usuário quanto de dinheiro
+ele tem e em seguida diga quantos litros de combustível ele
+pode comprar e quantos kilometros o carro consegue andar com este tanto de combustível.
+
+Seu script será usado no computador de bordo do carro. */
+
+let quiLitro = 20;
+let valorLitro = 5;
+let dinheiro = 43;
+let qntCombustivel = dinheiro / valorLitro;
+
+
+console.log(`O usuario tem ${dinheiro} reais em dinheiro`);
+console.log(`Pode comprar ${qntCombustivel}l de combustível`);
