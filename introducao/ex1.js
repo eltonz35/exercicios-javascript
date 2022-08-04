7988f1624c2c1394f283bc0f799ea15ac101a365
Elthon <eltonsilva.mecatronic@gmail.com>
2022-08-03 21:32:57 -0300

    introducao/ex1

---
+++ introducao/ex1.js
@@ -0,0 +1,13 @@
+/*Escreva um programa que pede o raio de um círculo,
+ e em seguida exiba o perímetro e área do círculo.
+
+Para saber o valor do pi em JavaScript, use:
+Math.PI */
+
+let raio = 3;
+let comprimento = 2 * Math.PI * raio;
+let area = Math.PI * raio * raio;
+
+console.log("Comprimento da circunferência é = " + comprimento);
+console.log("Area da circunferência é = " + area);
+
