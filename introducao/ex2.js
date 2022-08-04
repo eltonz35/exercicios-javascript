637d4e9704a14a466128243e22f08afd3e2b7721
Elthon <eltonsilva.mecatronic@gmail.com>
2022-08-03 21:41:08 -0300

    ex2.js

---
+++ introducao/ex2.js
@@ -0,0 +1,15 @@
+/*. Você está no Brasil, e para temperatura usamos o grau Celsius.
+Porém, quando você for contrato para trabalhar como programador Python 
+no exterior, deverá usar graus Fahrenheit.
+
+A fórmula da conversão é a seguinte: F = (9 / 5)* C + 32
+
+Tutorial completo de JavaScript
+ Ou seja, você fornece a temperatura em graus Celsius, e seu script JS
+ faz a conversão para graus Fahrenheit.
+ */
+
+let celsius = 27;
+let fahrenheit = (9 / 5) * celsius + 32;
+
+console.log("A temperatura em Fahrenheit é de " + fahrenheit);
