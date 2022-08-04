62ad8a286f2d7727797517a5ed5b1c167bd6ca60
Elthon <eltonsilva.mecatronic@gmail.com>
2022-08-03 21:47:34 -0300

    ex3.js

---
+++ introducao/ex3.js
@@ -0,0 +1,9 @@
+/* Agora faça o contrário. Você fornece a temperatura
+ em graus Fahrenheit, seu programa deve converter para
+Celsius e exibir na tela a temperatura em graus Celsius.
+ */
+
+let fahrenheit = 95;
+let celsius = (fahrenheit - 32) * (5 / 9);
+
+console.log("A temperatura em Celsius é de " + celsius);
