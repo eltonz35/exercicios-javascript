71013c3514cdb1f3ee09b0112784da8b9da0547e
Elthon <eltonsilva.mecatronic@gmail.com>
2022-08-04 16:55:57 -0300

    ex1

---
+++ ifelse/ex1.js
@@ -0,0 +1,10 @@
+/* Faça um script que verifique se uma letra digitada
+ num campo de input é vogal ou consoante. */
+
+ let letra = "o";
+
+ if(letra == "a" || letra == "e" || letra == "i" || letra == "o" || letra == "u") {
+    console.log(("A letra digitada é uma vogal!"));
+ } else {
+    console.log("A letra digitada é uma consoante!");
+ }
