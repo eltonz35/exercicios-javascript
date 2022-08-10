3eba2df8de7ef6231edc508ae9ab8cd5e75ecdd7
Elthon <eltonsilva.mecatronic@gmail.com>
2022-08-10 15:37:56 -0300

    ex3

---
+++ loops/ex3.js
@@ -0,0 +1,9 @@
+/*Faça um programa que imprima na tela os números de 1 a 20,
+ um abaixo do outro. Depois modifique o programa para que ele
+  mostre os números um ao lado do outro. */
+
+let numero;
+
+for(numero = 1; numero <= 20; numero++) {
+    console.log(numero);
+}
